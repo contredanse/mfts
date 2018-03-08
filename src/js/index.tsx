91b8d945acf813fa586e56941ed7b05518c5802a
belgattitude <belgattitude@gmail.com>
2018-03-08 16:49:04 +0100

    Inject config

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
-// import { ConnectedRouter } from 'react-router-redux'
 import store from './store';
 import App from './containers/app';
 
 import '@public/favicon.ico';
-//import 'typeface-roboto';
 import 'typeface-quicksand';
 
 import '@styles/style.scss';
+import {appConfig, AppConfig} from "@config/app-config";
 
-const renderApp = (Component: any, elementId: string) => {
+const renderApp = (App, appConfig: AppConfig, elementId: string) => {
     render(
         <ReduxProvider store={store}>
-                <Component/>
+            <App appConfig={appConfig}/>
         </ReduxProvider>,
         document.getElementById(elementId)
     );
 };
-
-renderApp(App, 'app');
+console.log('appConfig', appConfig);
+renderApp(App, appConfig, 'app');
