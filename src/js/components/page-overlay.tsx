1b7bf9e50a9b81d7592bc9443105cbb097a36d00
belgattitude <belgattitude@gmail.com>
2018-03-23 13:20:44 +0100

    temp fixes for page overlay

--- src/js/components/page-overlay.tsx
+++ src/js/components/page-overlay.tsx
@@ -47,6 +47,7 @@ export class PageOverlay extends React.Component<IProps, IState> {
 
         return (
             <div className="page-overlay-viewport">
+
                 <div className="page-overlay-ctn">
                     {closeButton === true && (
                         <div className="top-bar">
