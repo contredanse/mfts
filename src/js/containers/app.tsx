ad8d22e7e17ac6c9ebbcc3bc80c13ed34371e2cd
belgattitude <belgattitude@gmail.com>
2018-03-23 13:20:23 +0100

    Strat working on pages

--- src/js/containers/app.tsx
+++ src/js/containers/app.tsx
@@ -4,15 +4,16 @@ import { ConnectedRouter } from 'react-router-redux';
 import { Route } from 'react-router-dom';
 import { history } from '@src/store';
 
-import { Switch } from 'react-router';
+import {RouteComponentProps, Switch} from 'react-router';
 import { AppBarConnected } from '@src/components/app-bar-connected';
-import HomePage from '@src/containers/home-page';
-import IntroPage from '@src/containers/intro-page';
-import MenuPage from '@src/containers/menu-page';
-import NotFoundPage from '@src/containers/notfound-page';
-import VideoListPage from '@src/containers/video-list-page';
+import HomePage from '@src/containers/home-container';
+import IntroPage from '@src/containers/intro-container';
+import MenuPage from '@src/containers/menu-container';
+import NotFoundPage from '@src/containers/notfound-container';
+import VideoListPage from '@src/containers/video-list-container';
 import { AppConfig } from '@config/app-config';
-import PageListPage from '@src/containers/page-list-page';
+import PageListPage from '@src/containers/page-list-container';
+import PageContainer from "@src/containers/page-container";
 
 interface IAppProps {
     appConfig: AppConfig;
@@ -53,6 +54,17 @@ class App extends React.Component<IAppProps, {}> {
                                 render={props => (
                                     <VideoListPage initialData={data.videos} videosBaseUrl={videosBaseUrl} {...props} />
                                 )}
+                            />
+                            <Route
+                                exact={true}
+                                path="/page/:pageId"
+                                render={(props: RouteComponentProps<any>) => {
+                                    console.log('params', props);
+                                    const {params} = props.match;
+                                    return (
+                                        <PageContainer pageId={params.pageId} />
+                                    )
+                                }}
                             />
                             <Route component={NotFoundPage} />
                         </Switch>
