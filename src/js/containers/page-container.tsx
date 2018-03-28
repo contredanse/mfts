6f8c1e1b1b11bb7a27f863b954b192c93e7d45b0
belgattitude <belgattitude@gmail.com>
2018-03-28 17:34:14 +0200

    Moved logic for page loading check into PageContainer

--- src/js/containers/page-container.tsx
+++ src/js/containers/page-container.tsx
@@ -1,22 +1,61 @@
 import React from 'react';
 import Page from '@src/components/page';
 import { IDataPage } from '@data/data-pages';
+import PageRepository from '@src/repositories/page-repository';
+import NotFoundContainer from '@src/containers/notfound-container';
 
 interface IProps {
     pageId: string;
+    pageRepository: PageRepository;
 }
-interface IState {}
+interface IState {
+    pageExists: boolean|undefined;
+    pageData: IDataPage|undefined;
+}
 
 class PageContainer extends React.Component<IProps, IState> {
+
+    readonly state = {
+        pageExists: undefined,
+        pageData: undefined
+    }
+
     constructor(props) {
         super(props);
     }
 
-    componentDidMount() {}
+    async componentDidMount() {
+        try {
+            const pageData = await this.props.pageRepository.get(this.props.pageId);
+            this.setState((prevState: IState): IState => {
+                return {...prevState,
+                        pageExists: true,
+                        pageData: pageData
+                }
+            })
+        } catch (e) {
+            this.setState((prevState: IState): IState => {
+                return {...prevState,
+                        pageExists: false,
+                        pageData: undefined
+                }
+            })
+        }
+    }
 
     render() {
-        return (<Page pageId={this.props.pageId}/>);
-
+        const { pageExists, pageData } = this.state;
+        // should not be required, exit if async loading
+        // of pageData is not yet present (see componentDidMount())
+        if (pageExists === undefined) return null;
+        return (
+            <div>
+                {pageData ?
+                    <Page pageData={pageData}/> :
+                    <NotFoundContainer/>
+                }
+            </div>
+        );
     }
 }
 
