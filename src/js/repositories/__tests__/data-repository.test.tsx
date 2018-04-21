01ecd5625abed857f7e49b1868860b2ddd64ecf6
belgattitude <belgattitude@gmail.com>
2018-04-21 12:54:38 +0200

    data-repository tests additions

--- src/js/repositories/__tests__/data-repository.test.tsx
+++ src/js/repositories/__tests__/data-repository.test.tsx
@@ -1,4 +1,5 @@
 import DataRepository, { IParams } from '../data-repository';
+import { appConfig } from '@config/app-config';
 
 describe('Data repository params', () => {
     test('getParams returns params from constructor', () => {
@@ -14,12 +15,18 @@ describe('Data repository getters', () => {
     const params = {
         defaultLang: 'en',
     };
-    const data = [];
-    const globalDataRepo = new DataRepository(data, params);
+    const {pages: globalPageData} = appConfig.getConfig().data;
+    const globalDataRepo = new DataRepository(globalPageData, params);
 
     test('getPage should reject for non existing page', async () => {
         await expect(globalDataRepo.getPage('non-existing-page')).rejects.toThrow(
             "Page 'non-existing-page' cannot be found"
         );
     });
+
+    test('getPage existing page should have a name', async () => {
+        const page = await globalDataRepo.getPage('forms.helix-roll.in-water');
+        expect(page).toHaveProperty('name');
+    });
+
 });
