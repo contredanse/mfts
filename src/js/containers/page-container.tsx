5b688012c71aa53fc71b1dd4893233e740f6c94d
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-06-07 15:00:58 +0200

    PageOverlay moved one level up

--- src/js/containers/page-container.tsx
+++ src/js/containers/page-container.tsx
@@ -3,6 +3,7 @@ import Page from '@src/components/page';
 import NotFoundContainer from '@src/containers/notfound-container';
 import { DataSupportedLangType, IDataRepository } from '@src/models/repository/data-repository';
 import PageEntity from '@src/models/entity/page-entity';
+import { PageOverlay } from '@src/components/page-overlay';
 
 type PageContainerProps = {
     pageId: string;
@@ -57,7 +58,11 @@ class PageContainer extends React.Component<PageContainerProps, PageContainerSta
             return null;
         }
         return (
-            <div>{pageEntity ? <Page pageEntity={pageEntity} lang={this.props.lang} /> : <NotFoundContainer />}</div>
+            <PageOverlay closeButton={false}>
+                <div className="page-wrapper">
+                    {pageEntity ? <Page pageEntity={pageEntity} lang={this.props.lang} /> : <NotFoundContainer />}
+                </div>
+            </PageOverlay>
         );
     }
 }
