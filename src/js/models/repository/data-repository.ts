2cbddd6b162d80454607309ae6c4f2b674b8880e
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-08-28 13:26:53 +0200

    removed obsolete interface

--- src/js/models/repository/data-repository.ts
+++ src/js/models/repository/data-repository.ts
@@ -1,7 +1,3 @@
-import VideoEntity from '@src/models/entity/video-entity';
-import PageEntity from '@src/models/entity/page-entity';
-import { IJsonVideo } from '@data/json/data-videos';
-
 export type DataSupportedLangType = 'en' | 'fr'; // | 'nl' | 'de' | 'es';
 
 export interface IDataRepositoryParams {
@@ -9,11 +5,4 @@ export interface IDataRepositoryParams {
     assetsBaseUrl: string;
     videoBaseUrl: string;
     audioBaseUrl: string;
-}
-
-export interface IDataRepository {
-    //getVideoEntity(videoId: string): Promise<VideoEntity>;
-    getVideoEntity(videoId: string): VideoEntity | undefined;
-    getPageEntity(pageId: string): PageEntity | undefined;
-    getVideo(videoId: string): IJsonVideo | undefined;
 }
