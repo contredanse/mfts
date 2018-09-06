e7ce64080103e6886ac2ea64c75f63c3e916f4d6
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-06 22:58:50 +0200

    counter reducer for testing purposes

--- src/js/redux/root-reducer.ts
+++ src/js/redux/root-reducer.ts
@@ -1,13 +1,22 @@
-import { combineReducers } from 'redux';
-import { routerReducer, RouterState } from 'react-router-redux';
+import { Action, AnyAction, combineReducers, Dispatch } from 'redux';
+import counterReducer from './reducers/counter';
 
-interface StoreEnhancerState {}
-
-export interface RootState extends StoreEnhancerState {
-    router: RouterState;
+// The top-level state object
+//
+// `connected-react-router` already injects the router state typings for us,
+// so we can ignore them here.
+export interface RootState {
+    count: number;
 }
 
-import { RootAction } from '@src/redux';
-export const rootReducer = combineReducers<RootState, RootAction>({
-    router: routerReducer,
+// Additional props for connected React components. This prop is passed by default with `connect()`
+export interface ConnectedReduxProps<A extends Action = AnyAction> {
+    dispatch: Dispatch<A>;
+}
+
+// Whenever an action is dispatched, Redux will update each top-level application state property
+// using the reducer with the matching name. It's important that the names match exactly, and that
+// the reducer acts on the corresponding ApplicationState property type.
+export const rootReducer = combineReducers<RootState>({
+    count: counterReducer,
 });
