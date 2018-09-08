61a37af9b3b5d543d7bcd101580e7a02ab7f66d3
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-08 18:46:26 +0200

    example for location observer

--- src/js/redux/root-reducer.ts
+++ src/js/redux/root-reducer.ts
@@ -1,12 +1,25 @@
 import { Action, AnyAction, combineReducers, Dispatch } from 'redux';
 import counterReducer from './reducers/counter';
 
+import { LOCATION_CHANGE } from 'connected-react-router';
+
+const routerReducer = (state = {}, action: Action): any => {
+    switch (action.type) {
+        case LOCATION_CHANGE: {
+            console.log('new location', state);
+            return state;
+        }
+        default:
+            return state;
+    }
+};
+
 // The top-level state object
 //
 // `connected-react-router` already injects the router state typings for us,
 // so we can ignore them here.
 export interface RootState {
-    count: number;
+    //    count: number;
 }
 
 // Additional props for connected React components. This prop is passed by default with `connect()`
@@ -17,6 +30,7 @@ export interface ConnectedReduxProps<A extends Action = AnyAction> {
 // Whenever an action is dispatched, Redux will update each top-level application state property
 // using the reducer with the matching name. It's important that the names match exactly, and that
 // the reducer acts on the corresponding ApplicationState property type.
-export const rootReducer = combineReducers<RootState>({
+export const rootReducer = combineReducers<RootState, AnyAction>({
     count: counterReducer,
+    routerReducer: routerReducer,
 });
