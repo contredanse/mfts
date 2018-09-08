4935a8e701998551678f055c810bf7f06228ad8e
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-08 21:28:10 +0200

    removal of obsolete store implementation

--- src/js/redux/root-action.ts
+++
@@ -1,6 +0,0 @@
-import { RouterAction } from 'connected-react-router';
-import { LocationChangeAction } from '../../typings/connected-router-extra';
-
-type ReactRouterAction = RouterAction | LocationChangeAction;
-
-export type RootAction = ReactRouterAction;
--- src/js/redux/types.ts
+++
@@ -1,6 +0,0 @@
-import { Dispatch as ReduxDispatch, Reducer as ReduxReducer } from 'redux';
-
-import { RootState, RootAction } from '@src/redux';
-
-export type Dispatch = ReduxDispatch<RootAction>;
-export type Reducer = ReduxReducer<RootState, RootAction>;
