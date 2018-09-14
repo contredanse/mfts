3c466549c2168c82902a17a0d8793c7f8c194442
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-14 16:00:46 +0200

    new interfaces

--- src/data/json/data-menu.d.ts
+++ src/data/json/data-menu.d.ts
@@ -1,10 +1,53 @@
 ﻿type JsonMenuNodeType = 'page' | 'section';
 
-export interface IJsonMenu {
-    type: JsonMenuNodeType;
-    id: string;
-    page_id?: string;
+export type IJsonMenuLocalizedTitles = {
     title_en: string;
     title_fr: string;
+};
+
+export type IJsonMenuSection = {
+    id: string;
+    content?: IJsonMenu[];
+} & IJsonMenuLocalizedTitles;
+
+export type IJsonMenuPage = {
+    page_id: string;
+} & IJsonMenuLocalizedTitles;
+
+export type IJsonMenu = IJsonMenuSection &
+    IJsonMenuPage & {
+        type: JsonMenuNodeType;
+        id?: string;
+        page_id?: string;
+    };
+
+/*
+type JsonMenuNodeType = 'page' | 'section';
+
+
+export interface IJsonMenuSection {
+    id: string;
+}
+
+export interface IJsonMenuLocalizedTitles {
+    title_en: string;
+    title_fr: string;
+}
+
+export interface IJsonMenuSection extends IJsonMenuLocalizedTitles {
+    id: string;
     content?: IJsonMenu[];
 }
+
+
+export interface IJsonMenuPage extends IJsonMenuLocalizedTitles {
+    page_id: string;
+}
+
+
+export interface IJsonMenu extends IJsonMenuSection, IJsonMenuPage {
+    type: JsonMenuNodeType;
+    id?: string;
+    page_id?: string;
+}
+*/
