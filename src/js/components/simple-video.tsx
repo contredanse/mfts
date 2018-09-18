528be936e85cc008a63db8087a4117c0440581f9
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-18 13:43:31 +0200

    simple video component

---
+++ src/js/components/simple-video.tsx
@@ -0,0 +1,19 @@
+import { VideoHTMLAttributes } from 'react';
+import React from 'react';
+
+export type SimpleVideoProps = {
+    videoSrcs: Array<{ src: string; type: string }>;
+} & VideoHTMLAttributes<HTMLVideoElement>;
+
+const SimpleVideo: React.SFC<SimpleVideoProps> = (props: SimpleVideoProps) => {
+    const { videoSrcs, ...mediaProps } = props;
+    return (
+        <video {...mediaProps}>
+            {videoSrcs.map((v, idx) => (
+                <source key={idx} src={v.src} type={v.type} />
+            ))}
+        </video>
+    );
+};
+
+export default SimpleVideo;
