3de858563ca768dcc7118d43c484057d1a05fecc
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-19 20:16:55 +0200

    removed console stuff

--- src/js/components/player/utils.ts
+++ src/js/components/player/utils.ts
@@ -1,18 +1,13 @@
 export const hideAllSubtitles = (video: HTMLVideoElement): void => {
     for (let i = 0; i < video.textTracks.length; i++) {
-        console.log('Hidding', video.textTracks[i]);
         video.textTracks[i].mode = 'hidden';
-        console.log('New value', video.textTracks[i]);
     }
 };
 
 export const showSubtitle = (video: HTMLVideoElement, lang: string): void => {
     for (let i = 0; i < video.textTracks.length; i++) {
-        // For the 'subtitles-off' button, the first condition will never match so all will subtitles be turned off
         if (video.textTracks[i].language === lang) {
-            console.log('SHOWING', video.textTracks[i]);
             video.textTracks[i].mode = 'showing';
-            //this.setAttribute('data-state', 'active');
         } else {
             video.textTracks[i].mode = 'hidden';
         }
