724381ff7880617973e295a3e8271a435f01b3bf
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-19 20:11:12 +0200

    Prep about page

--- src/js/components/about.tsx
+++ src/js/components/about.tsx
@@ -14,18 +14,25 @@ class About extends React.PureComponent<AboutProps, AboutState> {
     }
 
     render() {
+        let video = 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4';
+        video = 'https://assets.materialforthespine.com/videos/intro_2tubes_walk.webm';
+
+        // <source src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" type="video/mp4" />
         return (
             <section className="fullsize-video-bg">
                 <div className="inner">
                     <div>
-                        <h1>Responsive Background Video</h1>
-                        <p>with color and dot-grid overlay</p>
+                        <h1>Material for the spine</h1>
+                        <p>
+                            <a href="#">A movement study</a>
+                            &nbsp;|&nbsp;
+                            <a href="#">Une étude du mouvement</a>
+                        </p>
                     </div>
                 </div>
                 <div id="video-viewport">
-                    <video width="1920" height="1280" autoPlay muted loop>
-                        <source src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" type="video/mp4" />
-                        <source src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm" type="video/webm" />
+                    <video autoPlay muted loop>
+                        <source src={video} />
                     </video>
                 </div>
             </section>
