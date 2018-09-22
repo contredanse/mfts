606cf9d2cdb8a2d00c369691d69ba4a438fbcf06
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-22 13:55:29 +0200

    add playbackRate support

--- src/js/components/simple-video.tsx
+++ src/js/components/simple-video.tsx
@@ -1,19 +1,48 @@
 import { VideoHTMLAttributes } from 'react';
 import * as React from 'react';
 
+export type VideoSourcesProps = Array<{ src: string; type: string }>;
+
 export type SimpleVideoProps = {
-    videoSrcs: Array<{ src: string; type: string }>;
+    srcs?: VideoSourcesProps;
+    playbackRate: number;
 } & VideoHTMLAttributes<HTMLVideoElement>;
 
-const SimpleVideo: React.SFC<SimpleVideoProps> = (props: SimpleVideoProps) => {
-    const { videoSrcs, ...mediaProps } = props;
-    return (
-        <video {...mediaProps}>
-            {videoSrcs.map((v, idx) => (
-                <source key={idx} src={v.src} type={v.type} />
-            ))}
-        </video>
-    );
-};
+const defaultProps = {
+    playbackRate: 1,
+} as SimpleVideoProps;
+
+class SimpleVideo extends React.PureComponent<SimpleVideoProps, {}> {
+    static readonly defaultProps: SimpleVideoProps = defaultProps;
+
+    protected videoRef!: React.RefObject<HTMLVideoElement>;
+
+    constructor(props: SimpleVideoProps) {
+        super(props);
+        this.videoRef = React.createRef<HTMLVideoElement>();
+    }
+
+    componentDidMount() {
+        const videoEl = this.getVideoElement();
+        const { playbackRate } = this.props;
+        if (videoEl !== null) {
+            videoEl.playbackRate = playbackRate;
+        }
+    }
+
+    getVideoElement(): HTMLVideoElement | null {
+        return this.videoRef.current;
+    }
+
+    render() {
+        const { srcs, ...mediaProps } = this.props;
+        return (
+            <video ref={this.videoRef} {...mediaProps}>
+                {srcs && srcs.map((v, idx) => <source key={idx} src={v.src} type={v.type} />)}
+                {this.props.children}
+            </video>
+        );
+    }
+}
 
 export default SimpleVideo;
