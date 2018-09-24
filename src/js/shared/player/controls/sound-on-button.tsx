7feac83d6963697516a0bc3251e7f3fa45bb5775
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-24 15:36:14 +0200

    fixed icon

--- src/js/shared/player/controls/sound-on-button.tsx
+++ src/js/shared/player/controls/sound-on-button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Button, { ButtonProps } from './button';
-import { VolumeMuteIcon as SoundOnIcon } from 'mdi-react';
+import { VolumeHighIcon as SoundOnIcon } from 'mdi-react';
 
 import './buttons.scss';
 
