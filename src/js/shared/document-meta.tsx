0e877c877d09f1d2f91e2cfb06f7b33fd7082c27
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-25 11:16:01 +0200

    Added document meta

---
+++ src/js/shared/document-meta.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+type DocumentMetaProps = {
+    title?: string;
+    children?: JSX.Element;
+};
+
+type DocumentMetaState = {};
+
+class DocumentMeta extends React.PureComponent<DocumentMetaProps, DocumentMetaState> {
+    constructor(props: DocumentMetaProps) {
+        super(props);
+    }
+
+    static updateDocumentMeta(props: DocumentMetaProps): void {
+        const { title } = props;
+        if (title && document && 'title' in document) {
+            document.title = title;
+        }
+    }
+
+    componentDidMount() {
+        DocumentMeta.updateDocumentMeta(this.props);
+    }
+
+    componentDidUpdate() {
+        DocumentMeta.updateDocumentMeta(this.props);
+    }
+
+    render() {
+        const { children } = this.props;
+        if (children === undefined) {
+            return null;
+        }
+        return <>{children}</>;
+    }
+}
+
+export default DocumentMeta;
