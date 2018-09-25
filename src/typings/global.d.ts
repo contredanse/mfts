c4d35ab1b08367e4ae3f9a59cd60056b6252ae4f
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-25 14:20:05 +0200

    add md/markdown module resolution

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -9,6 +9,11 @@ declare interface ServiceWorkerConfig {
     onUpdate: (registration: ServiceWorkerRegistration) => void;
 }
 
+declare module '*.md' {
+    const value: string;
+    export default value;
+}
+
 declare module '*.png' {
     const value: string;
     export default value;
