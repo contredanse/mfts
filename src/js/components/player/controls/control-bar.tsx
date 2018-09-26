00dbf56175ef98a628b4af0919c0922f12db6d79
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-26 18:47:33 +0200

    updated control with provider

--- src/js/components/player/controls/control-bar.tsx
+++ src/js/components/player/controls/control-bar.tsx
@@ -19,6 +19,8 @@ import {
     showSubtitle,
 } from '@src/components/player/controls/utils/subtitles-actions';
 
+import PlaybackStatusProvider from '@src/components/player/controls/hoc/playback-status-provider';
+
 export type MediaPlayerControlBarProps = {
     videoEl?: HTMLVideoElement;
     duration: number;
@@ -37,11 +39,7 @@ export type MediaPlayerControlbarState = {
     currentTime: number;
     bufferTime: number;
     isActive: boolean;
-    isPlaying: boolean;
     intervalWhilePlaying: number;
-    muted: boolean;
-    volume: number;
-    isLoading: boolean;
 };
 
 export class ControlBar extends React.PureComponent<MediaPlayerControlBarProps, MediaPlayerControlbarState> {
@@ -63,49 +61,10 @@ export class ControlBar extends React.PureComponent<MediaPlayerControlBarProps, 
         super(props);
         this.state = {
             isActive: true,
-            isPlaying: false,
             currentTime: 0,
             bufferTime: 0,
             intervalWhilePlaying: 0,
-            muted: false,
-            volume: 1.0,
-            isLoading: true,
         };
-    }
-
-    componentDidMount() {
-        // If videoEl is initially available, let's register listeners at mount
-        if (this.props.videoEl) {
-            const { videoEl } = this.props;
-            this.registerVideoListeners(videoEl);
-            this.setState(prevState => ({
-                ...prevState,
-                playing: !videoEl.paused,
-                muted: videoEl.muted,
-                volume: videoEl.volume,
-                currentTime: videoEl.currentTime || 0,
-                isLoading: videoEl.readyState <= 2,
-            }));
-            //this.progressBar = withVideoProgress(NewProgressBar);
-        }
-    }
-
-    componentDidUpdate(prevProps: MediaPlayerControlBarProps, prevState: MediaPlayerControlbarState): void {
-        // In case of videoEl was not available at initial render
-        // listeners will be initialized at update
-        if (!prevProps.videoEl && this.props.videoEl) {
-            this.registerVideoListeners(this.props.videoEl);
-        }
-    }
-
-    componentWillUnmount() {
-        // Removing the video listeners if they were registered
-        if (this.props.videoEl && this.listenersRegistered) {
-            this.unregisterVideoListeners(this.props.videoEl);
-            this.listenersRegistered = false;
-        }
-
-        // to handle autoHide: clearInterval(this.interval);
     }
 
     handleEnableHover = (e: MouseEvent<HTMLDivElement>): void => {
@@ -127,22 +86,6 @@ export class ControlBar extends React.PureComponent<MediaPlayerControlBarProps, 
             border: '3px solid yellow',
         };
 
-        const { muted, isLoading, isPlaying } = this.state;
-
-        const overlayWrapper = () => {
-            return (
-                <div
-                    className={'control-bar-overlay' + (!isPlaying ? ' control-bar-overlay--active' : '')}
-                    onMouseOver={this.handleEnableHover}
-                    onMouseOut={this.handleDisableHover}
-                >
-                    <div className="control-bar-overlay-top" />
-                    <div className="control-bar-overlay-middle" />
-                    <div className="control-bar-overlay-bottom">Hello the controbar</div>
-                </div>
-            );
-        };
-
         const LoadingIndicator = () => <div>Loading...</div>;
 
         return (
@@ -152,60 +95,50 @@ export class ControlBar extends React.PureComponent<MediaPlayerControlBarProps, 
                         <ProgressBar videoEl={videoEl} progressInterval={650} isSeekable={true} onSeek={this.seekTo} />
                     )}
                 </div>
-                <div className="control-bar-ctn__panel">
-                    <div className="control-bar-ctn__panel__left">
-                        {!isPlaying && <PlayButton isEnabled={true} onClick={this.play} />}
-                        {isPlaying && <PauseButton isEnabled={true} onClick={this.pause} />}
-                        {muted ? (
-                            <SoundOnButton isEnabled={true} onClick={this.unMute} />
-                        ) : (
-                            <SoundOffButton isEnabled={true} onClick={this.mute} />
-                        )}
-                        {isLoading && <LoadingIndicator />}
-                    </div>
+                <PlaybackStatusProvider videoEl={videoEl}>
+                    {status => (
+                        <div className="control-bar-ctn__panel">
+                            <div className="control-bar-ctn__panel__left">
+                                {status.isPlaying ? (
+                                    <PauseButton isEnabled={true} onClick={this.pause} />
+                                ) : (
+                                    <PlayButton isEnabled={true} onClick={this.play} />
+                                )}
+                                {status.muted ? (
+                                    <SoundOnButton isEnabled={true} onClick={this.unMute} />
+                                ) : (
+                                    <SoundOffButton isEnabled={true} onClick={this.mute} />
+                                )}
+                                {status.isLoading && <LoadingIndicator />}
+                            </div>
 
-                    <div className="control-bar-ctn__panel__right">
-                        {props.enableSpeedControl && <PlaybackRateSelect onChange={props.actions.setPlaybackRate} />}
-                        <SubtitlesButton isEnabled={true} onClick={this.toggleSubtitles} />
-                        {props.enablePrevControl && (
-                            <PrevButton
-                                isEnabled={this.props.onPreviousLinkPressed !== undefined}
-                                onClick={() => {
-                                    this.props.onPreviousLinkPressed!();
-                                }}
-                            />
-                        )}
-                        {props.enableNextControl && (
-                            <NextButton
-                                isEnabled={this.props.onNextLinkPressed !== undefined}
-                                onClick={() => {
-                                    this.props.onNextLinkPressed!();
-                                }}
-                            />
-                        )}
-                    </div>
-                </div>
+                            <div className="control-bar-ctn__panel__right">
+                                {props.enableSpeedControl && (
+                                    <PlaybackRateSelect onChange={props.actions.setPlaybackRate} />
+                                )}
+                                <SubtitlesButton isEnabled={true} onClick={this.toggleSubtitles} />
+                                {props.enablePrevControl && (
+                                    <PrevButton
+                                        isEnabled={this.props.onPreviousLinkPressed !== undefined}
+                                        onClick={() => {
+                                            this.props.onPreviousLinkPressed!();
+                                        }}
+                                    />
+                                )}
+                                {props.enableNextControl && (
+                                    <NextButton
+                                        isEnabled={this.props.onNextLinkPressed !== undefined}
+                                        onClick={() => {
+                                            this.props.onNextLinkPressed!();
+                                        }}
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    )}
+                </PlaybackStatusProvider>
             </div>
         );
-    }
-
-    protected registerVideoListeners(video: HTMLVideoElement, skipOnRegistered: boolean = true): void {
-        if (skipOnRegistered && this.listenersRegistered) {
-            return;
-        }
-        video.addEventListener('volumechange', this.updateVolumeState);
-        video.addEventListener('playing', this.updatePlayingState);
-        video.addEventListener('pause', this.updatePlayingState);
-        video.addEventListener('waiting', this.setLoadingState);
-        this.listenersRegistered = true;
-    }
-
-    protected unregisterVideoListeners(video: HTMLVideoElement): void {
-        video.removeEventListener('volumechange', this.updateVolumeState);
-        video.removeEventListener('playing', this.updatePlayingState);
-        video.removeEventListener('pause', this.updatePlayingState);
-        video.removeEventListener('waiting', this.setLoadingState);
-        this.listenersRegistered = false;
     }
 
     protected toggleSubtitles = (): void => {
@@ -216,57 +149,6 @@ export class ControlBar extends React.PureComponent<MediaPlayerControlBarProps, 
             } else {
                 showSubtitle(videoEl, 'fr');
             }
-        }
-    };
-
-    /**
-     * Update local state with volume and mute
-     * @param {Event<HTMLVideoElement>} e
-     */
-    protected updateVolumeState = (e: Event): void => {
-        const { videoEl } = this.props;
-
-        if (videoEl && e.target !== null) {
-            if (this.state.muted !== videoEl.muted || this.state.volume !== videoEl.volume) {
-                this.setState({
-                    volume: videoEl.volume,
-                    muted: videoEl.muted,
-                });
-            }
-        } else {
-            console.warn('Cannot update volumeState, no "event.target" available', e);
-        }
-    };
-
-    /**
-     * Update local state with loading state
-     * @param {Event<HTMLVideoElement>} e
-     */
-    protected setLoadingState = (e: Event): void => {
-        const { videoEl } = this.props;
-        if (videoEl && e.target !== null) {
-            this.setState({
-                isLoading: true,
-            });
-        } else {
-            console.warn('Cannot update loadingState, no "event.target" available', e);
-        }
-    };
-
-    /**
-     * Update local state with loading state
-     * @param {Event<HTMLVideoElement>} e
-     */
-    protected updatePlayingState = (e: Event): void => {
-        console.log('UPDATEPLAYINGSTATE');
-        const { videoEl } = this.props;
-        if (videoEl && e.target !== null) {
-            this.setState({
-                isPlaying: !videoEl.paused,
-                isLoading: false,
-            });
-        } else {
-            console.warn('Cannot update playingState, no "event.target" available', e);
         }
     };
 
@@ -337,5 +219,4 @@ export class ControlBar extends React.PureComponent<MediaPlayerControlBarProps, 
     }
 }
 
-//export default withVideoProgress(ControlBar);
 export default ControlBar;
