d5704b17d5210bc1a5d91cce3a2623a18de846b4
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-26 13:10:20 +0200

    added subtitle button component

---
+++ src/js/shared/player/controls/subtitles-button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Button, { ButtonProps } from './button';
+
+import { SubtitlesIcon } from 'mdi-react';
+
+import './buttons.scss';
+
+class SubtitlesButton extends Button {
+    static defaultProps: Partial<ButtonProps> = {
+        ...Button.defaultProps,
+        className: 'SubtitlesButton',
+        children: <SubtitlesIcon size="100%" />,
+        isEnabled: false,
+    };
+}
+
+export default SubtitlesButton;
