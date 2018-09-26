2c2bd9263b717af82e9b081e8c3db89e84ecc7ac
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-26 14:54:31 +0200

    Refactor controlbar buttons

---
+++ src/js/shared/player/controls/svg-mdi-button/index.ts
@@ -0,0 +1,8 @@
+export { default as Button, ButtonProps } from '../button';
+export { default as PlayButton } from './play-button';
+export { default as PauseButton } from './pause-button';
+export { default as PrevButton } from './prev-button';
+export { default as NextButton } from './next-button';
+export { default as SoundOffButton } from './sound-off-button';
+export { default as SoundOnButton } from './sound-on-button';
+export { default as SubtitlesButton } from './subtitles-button';
