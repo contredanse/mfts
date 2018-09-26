cc342a444c02f0690a51c79fbda3ae28dc635609
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-26 23:47:07 +0200

    added svg module

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -14,6 +14,11 @@ declare module '*.md' {
     export default value;
 }
 
+declare module '*.svg' {
+    const value: string;
+    export default value;
+}
+
 declare module '*.png' {
     const value: string;
     export default value;
