69fc7b8f95cde8ea553f58f75e5de2c87e7a67ff
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-26 22:00:28 +0200

    blacklist known files

--- src/js/service-worker.webpack.stub.js
+++ src/js/service-worker.webpack.stub.js
@@ -1,7 +1,7 @@
 workbox.skipWaiting();
 workbox.clientsClaim();
-//workbox.routing.registerNavigationRoute('index.html', {
-//    whitelist: [/^(?!\/__).*/],
-//});
-workbox.routing.registerNavigationRoute('index.html');
+workbox.routing.registerNavigationRoute('index.html', {
+    //whitelist: [/^(?!\/__).*/],
+    blacklist: [/\.(js|json|html|css|ico|jpe?g|png|svg|mp4|webm|vtt|md)$/],
+});
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
