df96a12324a2d8f9a9deb57c7bc0e8499ce3ab83
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-09-26 20:01:43 +0200

    Fixed whitelist issue

--- src/js/service-worker.webpack.stub.js
+++ src/js/service-worker.webpack.stub.js
@@ -1,6 +1,7 @@
 workbox.skipWaiting();
 workbox.clientsClaim();
-workbox.routing.registerNavigationRoute('index.html', {
-    whitelist: [/^(?!\/__).*/],
-});
+//workbox.routing.registerNavigationRoute('index.html', {
+//    whitelist: [/^(?!\/__).*/],
+//});
+workbox.routing.registerNavigationRoute('index.html');
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
