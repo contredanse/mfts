6333789d4e17a2edc62cb2a807bbd810893e0a2a
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-10-01 13:46:18 +0200

    updated basic video player

--- src/js/components/player/basic-video-player.tsx
+++ src/js/components/player/basic-video-player.tsx
@@ -1,24 +1,35 @@
-import React, { SourceHTMLAttributes, TrackHTMLAttributes, VideoHTMLAttributes } from 'react';
+import React, { SourceHTMLAttributes, SyntheticEvent, TrackHTMLAttributes, VideoHTMLAttributes } from 'react';
 import { Omit } from 'utility-types';
 
 export type VideoSourcesProps = Array<SourceHTMLAttributes<HTMLSourceElement>>;
 export type TracksSourcesProps = Array<TrackHTMLAttributes<HTMLTrackElement>>;
 
+export type BasicVideoActions = {
+    //onEnded?: (e: Event) => void;
+    onEnded?: (e: SyntheticEvent<HTMLVideoElement>) => void;
+};
+
 export type BasicVideoProps = {
     srcs?: VideoSourcesProps;
     tracks?: TracksSourcesProps;
     playbackRate: number;
-} & Omit<VideoHTMLAttributes<HTMLVideoElement>, 'src'>;
+} & Omit<VideoHTMLAttributes<HTMLVideoElement>, 'src' | 'onEnded'> &
+    BasicVideoActions;
+
+export type BasicVideoState = {
+    listenersRegistered: boolean;
+};
 
 const defaultProps = {
     playbackRate: 1,
     playsInline: true,
 };
 
-class BasicVideoPlayer extends React.Component<BasicVideoProps, {}> {
+class BasicVideoPlayer extends React.Component<BasicVideoProps, BasicVideoState> {
     static defaultProps = defaultProps;
 
     protected videoRef!: React.RefObject<HTMLVideoElement>;
+    protected listenersRegistered = false;
 
     constructor(props: BasicVideoProps) {
         super(props);
@@ -26,8 +37,20 @@ class BasicVideoPlayer extends React.Component<BasicVideoProps, {}> {
     }
 
     componentDidMount() {
-        const videoEl = this.getVideoElement();
         this.setPlaybackRate(this.props.playbackRate);
+        if (this.videoRef.current !== null) {
+            this.registerVideoListeners(this.videoRef.current);
+        } else {
+            throw Error('Registering listeners failed, video element is null');
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.videoRef.current !== null) {
+            this.unregisterVideoListeners(this.videoRef.current);
+        } else {
+            throw Error('Unregistering listeners failed, video element is null');
+        }
     }
 
     shouldComponentUpdate(nextProps: BasicVideoProps): boolean {
@@ -51,7 +74,14 @@ class BasicVideoPlayer extends React.Component<BasicVideoProps, {}> {
     }
 
     render() {
-        const { srcs, tracks, playbackRate, ...mediaProps } = this.props;
+        const {
+            srcs,
+            tracks,
+            // Just to omit those props
+            playbackRate,
+            // onEnded,
+            ...mediaProps
+        } = this.props;
         return (
             <video
                 ref={this.videoRef}
@@ -62,6 +92,25 @@ class BasicVideoPlayer extends React.Component<BasicVideoProps, {}> {
                 {tracks && tracks.map((t, idx) => <track key={idx} {...t} />)}
             </video>
         );
+    }
+
+    protected registerVideoListeners(video: HTMLVideoElement, skipOnRegistered: boolean = true): void {
+        if (skipOnRegistered && this.listenersRegistered) {
+            return;
+        }
+        const { onEnded } = this.props;
+        if (onEnded) {
+            //video.addEventListener('ended', onEnded);
+        }
+        //video.addEventListener('ratechange', this.updateVolumeState);
+        this.listenersRegistered = true;
+    }
+
+    protected unregisterVideoListeners(video: HTMLVideoElement): void {
+        if (this.listenersRegistered) {
+            //video.removeEventListener('ratechange', this.updateVolumeState);
+            this.listenersRegistered = false;
+        }
     }
 }
 
