2987be449ff0d55e87252dfd1f430c641a5a10e2
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-10-10 17:49:46 +0200

    added track manager

---
+++ src/js/components/player/track/html-video-track-manager.ts
@@ -0,0 +1,74 @@
+export type TextTrackKind = 'subtitles' | 'captions' | 'descriptions' | 'chapters' | 'metadata';
+
+export type TextTrackHTMLAttributes = {
+    kind: TextTrackKind;
+    src: string;
+    srcLang: string;
+    label: string;
+    default?: boolean;
+};
+
+export default class HTMLVideoTrackManager {
+    private videoEl: HTMLVideoElement;
+
+    constructor(videoEl: HTMLVideoElement) {
+        this.videoEl = videoEl;
+    }
+
+    static createTrackNode(textTrack: TextTrackHTMLAttributes): HTMLTrackElement {
+        const track = document.createElement('track') as HTMLTrackElement;
+        track.setAttribute('kind', textTrack.kind);
+        track.setAttribute('label', textTrack.label);
+        track.setAttribute('srcLang', textTrack.srcLang);
+        track.setAttribute('src', textTrack.src);
+        if (textTrack.default) {
+            track.setAttribute('default', 'default');
+        }
+        return track;
+    }
+
+    /**
+     * Return all HTMLTrackElement nodes associated
+     * to this video element.
+     */
+    getVideoTrackNodes(): NodeListOf<HTMLTrackElement> {
+        return this.videoEl.querySelectorAll('track');
+    }
+
+    /**
+     * Removes all HTMLTrackElement nodes under video element
+     * @return {number} number of tracks removed
+     */
+    removeVideoTrackNodes(): number {
+        let countRemoved = 0;
+        this.getVideoTrackNodes().forEach((trackNode: HTMLTrackElement) => {
+            this.videoEl.removeChild(trackNode);
+            countRemoved++;
+        });
+        return countRemoved;
+    }
+
+    /**
+     * @param textTracks
+     */
+    appendTrackNodes(textTracks: TextTrackHTMLAttributes[]): HTMLTrackElement[] {
+        const trackNodes: HTMLTrackElement[] = [];
+        textTracks.forEach(textTrack => {
+            const trackNode = HTMLVideoTrackManager.createTrackNode(textTrack);
+            trackNode.addEventListener('load', (e: Event) => {
+                const currentTrack = e.currentTarget as HTMLTrackElement;
+                if (currentTrack.default) {
+                    currentTrack.track.mode = 'showing';
+                    // thanks Firefox (not needed anymore)
+                    //video.textTracks[trackIdx].mode = 'showing';
+                } else {
+                    currentTrack.track.mode = 'hidden';
+                    // thanks Firefox (not needed anymore)
+                    //video.textTracks[trackIdx].mode = 'hidden';
+                }
+            });
+            trackNodes.push(this.videoEl.appendChild(trackNode));
+        });
+        return trackNodes;
+    }
+}
