07f29bb5c6c5e04cb1e7a7ca5e31bdb7918bc2ed
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-10-10 19:12:56 +0200

    Updated to terser plugin for es6 support

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -207,22 +207,55 @@ module.exports = merge(common, {
         noEmitOnErrors: true, // NoEmitOnErrorsPlugin
         //concatenateModules: true, //ModuleConcatenationPlugin (scope-hoisting)
         minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: true, // set to true if you want JS source maps
-                uglifyOptions: {
-                    output: {
-                        comments: false,
+            new TerserPlugin({
+                terserOptions: {
+                    parse: {
+                        // we want terser to parse ecma 8 code. However, we don't want it
+                        // to apply any minfication steps that turns valid ecma 5 code
+                        // into invalid ecma 5 code. This is why the 'compress' and 'output'
+                        // sections only apply transformations that are ecma 5 safe
+                        // https://github.com/facebook/create-react-app/pull/4234
+                        ecma: 8,
                     },
                     compress: {
+                        ecma: 5,
+
                         dead_code: true,
                         drop_debugger: true,
-                        drop_console: true,
+
+                        // Allow console messages
+                        drop_console: false,
+
+                        warnings: false,
+                        // Disabled because of an issue with Uglify breaking seemingly valid code:
+                        // https://github.com/facebook/create-react-app/issues/2376
+                        // Pending further investigation:
+                        // https://github.com/mishoo/UglifyJS2/issues/2011
+                        comparisons: false,
+                        // Disabled because of an issue with Terser breaking valid code:
+                        // https://github.com/facebook/create-react-app/issues/5250
+                        // Pending futher investigation:
+                        // https://github.com/terser-js/terser/issues/120
+                        inline: 2,
+                    },
+                    mangle: {
+                        safari10: true,
+                    },
+                    output: {
+                        ecma: 5,
+                        comments: false,
+                        // Turned on because emoji and regex is not minified properly using default
+                        // https://github.com/facebook/create-react-app/issues/2488
+                        ascii_only: true,
                     },
                 },
+                // Use multi-process parallel running to improve the build speed
+                // Default number of concurrent runs: os.cpus().length - 1
+                parallel: true,
+                // Enable file caching
+                cache: true,
+                sourceMap: true,
             }),
-
             new OptimizeCssAssetsPlugin({
                 cssProcessor: require('cssnano'),
                 cssProcessorOptions: {
