6813f2d5ec4bae16468e98254d92b29e08211dec
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-10-11 14:36:03 +0200

    removed obsolete service worker stub

--- src/js/service-worker.webpack.stub.js
+++
@@ -1,7 +0,0 @@
-workbox.skipWaiting();
-workbox.clientsClaim();
-workbox.routing.registerNavigationRoute('index.html', {
-    //whitelist: [/^(?!\/__).*/],
-    blacklist: [/\.(js|json|html|css|ico|jpe?g|png|svg|mp4|webm|vtt|md)$/],
-});
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
