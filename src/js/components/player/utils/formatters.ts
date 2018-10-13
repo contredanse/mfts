97d206f5c5a110a15ec6bf18ffde9748d14c0c40
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-10-13 11:01:27 +0200

    added utils for formatting time

---
+++ src/js/components/player/utils/formatters.ts
@@ -0,0 +1,12 @@
+export const formatTimeFromMilli = (milli: number): string => {
+    const d = Math.trunc(milli);
+    const h = Math.floor(d / 3600);
+    const m = Math.floor((d % 3600) / 60);
+    const s = Math.floor((d % 3600) % 60);
+    const minutes = m.toString().padStart(h > 0 ? 2 : 1, '0');
+    const seconds = s.toString().padStart(2, '0');
+    const hDisplay = h > 0 ? `${h}:` : '';
+    const mDisplay = m > 0 ? `${minutes}:` : `${'0'.padStart(m > 0 ? 2 : 1, '0')}:`;
+    const sDisplay = s > 0 ? `${seconds}` : '00';
+    return `${hDisplay}${mDisplay}${sDisplay}`;
+};
