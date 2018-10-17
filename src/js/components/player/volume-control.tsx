7012141b4b0b58d1b1ccb755c434f1842eae1a18
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-10-17 11:23:51 +0200

    added component for volume control

---
+++ src/js/components/player/volume-control.tsx
@@ -0,0 +1,50 @@
+import React, { PureComponent, CSSProperties } from 'react';
+import { SoundOffButton, SoundOnButton } from '@src/components/player/controls/svg-mdi-button';
+
+export type VolumeControlProps = {
+    muted: boolean;
+    volume: number;
+    mediaIsSilent?: boolean;
+    onUnMute: () => void;
+    onMute: () => void;
+    disableSpaceClick?: boolean;
+    isEnabled?: boolean;
+    style?: CSSProperties;
+};
+
+const defaultProps = {
+    isEnabled: true,
+    disableSpaceClick: false,
+    mediaIsSilent: false,
+    style: {},
+};
+
+class VolumeControl extends PureComponent<VolumeControlProps> {
+    static defaultProps = defaultProps;
+
+    render() {
+        const { style, muted, onMute, onUnMute, disableSpaceClick, isEnabled, mediaIsSilent } = this.props;
+
+        return (
+            <>
+                {muted ? (
+                    <SoundOffButton
+                        style={style}
+                        isEnabled={isEnabled}
+                        onClick={onUnMute}
+                        disableSpaceClick={disableSpaceClick}
+                    />
+                ) : (
+                    <SoundOnButton
+                        style={style}
+                        isEnabled={isEnabled}
+                        onClick={onMute}
+                        disableSpaceClick={disableSpaceClick}
+                    />
+                )}
+            </>
+        );
+    }
+}
+
+export default VolumeControl;
