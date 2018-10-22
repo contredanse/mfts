a566ac61fe434ad837d585e155d26ed7e89b0a9f
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-10-22 19:51:21 +0200

    first first rendering issue (the domNode is only in vdom)

--- src/js/components/navigation/app-bar-portal.tsx
+++ src/js/components/navigation/app-bar-portal.tsx
@@ -13,15 +13,33 @@ const defaultProps = {
 export default class AppBarPortal extends React.PureComponent<AppBarPortalProps, {}> {
     static defaultProps = defaultProps;
 
+    el: HTMLDivElement;
+
+    constructor(props: AppBarPortalProps) {
+        super(props);
+        this.el = document.createElement('div');
+    }
+
+    componentDidMount() {
+        const { container } = this.props;
+        const domNode = document.getElementById(container!);
+        if (domNode) {
+            domNode.appendChild(this.el);
+        } else {
+            console.warn(`Cannot mount appbarportal, container ${container} is not in the dom`);
+        }
+    }
+
+    componentWillUnmount() {
+        const { container } = this.props;
+        const domNode = document.getElementById(container!);
+        if (domNode) {
+            domNode.removeChild(this.el);
+        }
+    }
+
     render() {
         const { children, container } = this.props;
-        const domNode = document.getElementById(container!);
-
-        if (!domNode) {
-            console.warn(`Cannot mount appbarportal, container ${container} is not in the dom`);
-            return null;
-        }
-
-        return createPortal(children, domNode);
+        return createPortal(children, this.el);
     }
 }
