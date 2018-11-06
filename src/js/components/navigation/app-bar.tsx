eac64687bd549e8afd8f7bab7b04c97200c2ce9d
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-11-06 13:40:30 +0100

    appbar hidden is menu is open

--- src/js/components/navigation/app-bar.tsx
+++ src/js/components/navigation/app-bar.tsx
@@ -8,6 +8,9 @@ import './app-bar.scss';
 import { MenuIcon } from 'mdi-react';
 
 import helixSvg from '@assets/svg/helix-contredanse.svg';
+import { ApplicationState } from '@src/store';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
 
 type MenuLinkProps = {
     path: string;
@@ -18,13 +21,18 @@ type MenuLinkProps = {
 type AppBarProps = {
     title: string;
     lang: string;
+    hidden?: boolean;
     onMenuOpenRequest?: () => void;
 };
 
 export type AppBarWithRouterProps = AppBarProps & RouteComponentProps<{}>;
 
 export const AppBarComponent: React.SFC<AppBarWithRouterProps> = props => {
-    const { history, lang, onMenuOpenRequest } = props;
+    const { history, lang, onMenuOpenRequest, hidden } = props;
+
+    if (hidden) {
+        return null;
+    }
 
     return (
         <div className="app-bar-container">
@@ -55,4 +63,22 @@ export const AppBarComponent: React.SFC<AppBarWithRouterProps> = props => {
     );
 };
 
-export const AppBar = withRouter(AppBarComponent);
+const mapStateToProps = ({ ui }: ApplicationState) => ({
+    hidden: ui.isMenuOpen,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({});
+
+/*
+const ConnectedControlBar = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ControlBar);
+
+export default ConnectedControlBar;
+*/
+
+export const AppBar = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withRouter(AppBarComponent));
