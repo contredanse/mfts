4024bef6e5a88f42a91ddd36d266198a6b721ecd
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-11-06 13:03:09 +0100

    simplified new top bar

--- src/js/components/navigation/app-bar.tsx
+++ src/js/components/navigation/app-bar.tsx
@@ -1,29 +1,11 @@
-/**
- * Temp debug menu
- */
 import React from 'react';
 
 import { withRouter } from 'react-router';
 import { RouteComponentProps } from 'react-router';
 
-import {
-    AppBar as MaterialAppBar,
-    Toolbar,
-    IconButton,
-    withStyles,
-    WithStyles,
-    Theme,
-    createStyles,
-    Button,
-} from '@material-ui/core';
+import './app-bar.scss';
 
-import {
-    Menu as MenuIcon,
-    AccountBoxRounded as AccountBoxIcon,
-    InfoRounded as InfoIcon,
-    ListRounded as ListIcon,
-} from '@material-ui/icons';
-import ConnectedLangSelector from '@src/components/lang-selector';
+import { MenuIcon } from 'mdi-react';
 
 import helixSvg from '@assets/svg/helix-contredanse.svg';
 
@@ -37,66 +19,40 @@ type AppBarProps = {
     title: string;
     lang: string;
     onMenuOpenRequest?: () => void;
-    // menuLinks: MenuLinkProps[];
 };
 
-const styles = (theme: Theme) =>
-    createStyles({
-        root: {
-            width: '100%',
-        },
-        flex: {
-            flex: 1,
-        },
-        menuButton: {
-            marginLeft: -5,
-            marginRight: 0,
-        },
-    });
-
-export type AppBarWithStylesProps = AppBarProps & WithStyles<typeof styles>;
 export type AppBarWithRouterProps = AppBarProps & RouteComponentProps<{}>;
 
-export const AppBarComponent: React.SFC<AppBarWithStylesProps & AppBarWithRouterProps> = props => {
-    const { classes, history, lang, onMenuOpenRequest } = props;
-    //const currentPath = props.location.pathname;
+export const AppBarComponent: React.SFC<AppBarWithRouterProps> = props => {
+    const { history, lang, onMenuOpenRequest } = props;
 
     return (
-        <div className={classes.root}>
-            <MaterialAppBar elevation={0} position="fixed" style={{ backgroundColor: 'transparent' }}>
-                <Toolbar>
-                    <IconButton
-                        className={classes.menuButton}
-                        color="inherit"
-                        aria-label="Menu"
-                        onClick={() => {
-                            if (onMenuOpenRequest) {
-                                onMenuOpenRequest();
-                            } else {
-                                history.push('/');
-                            }
-                        }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
+        <div className="app-bar-container">
+            <button
+                className="mdi-icon"
+                onClick={() => {
+                    if (onMenuOpenRequest) {
+                        onMenuOpenRequest();
+                    } else {
+                        history.push('/');
+                    }
+                }}
+            >
+                <MenuIcon />
+            </button>
 
-                    <IconButton
-                        className={classes.menuButton}
-                        color="inherit"
-                        aria-label="Menu"
-                        onClick={() => {
-                            history.push(`/${lang}/menu`);
-                        }}
-                    >
-                        <img src={helixSvg} style={{ fill: 'white' }} />
-                        {/* <DnaIcon size={18} /> */}
-                    </IconButton>
+            <button
+                className="mdi-icon"
+                onClick={() => {
+                    history.push(`/${lang}/menu`);
+                }}
+            >
+                <img src={helixSvg} style={{ fill: 'white' }} />
+            </button>
 
-                    <div id="app-bar-portal-ctn" />
-                </Toolbar>
-            </MaterialAppBar>
+            <div id="app-bar-portal-ctn" />
         </div>
     );
 };
 
-export const AppBar = withStyles(styles)(withRouter(AppBarComponent));
+export const AppBar = withRouter(AppBarComponent);
