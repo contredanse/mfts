61ac5509aafccf8dde7db01d97f6c1c38f48fc60
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-11-14 18:11:19 +0100

    wip connected auth form

--- src/js/components/login-form.tsx
+++ src/js/components/login-form.tsx
@@ -2,17 +2,122 @@ import React from 'react';
 import './login-form.scss';
 import { RouteComponentProps } from 'react-router';
 import contredanseLogo from '@assets/images/logo-contredanse.png';
+import { ApplicationState } from '@src/store';
+import { Dispatch } from 'redux';
+import * as uiActions from '@src/store/ui/actions';
+import { connect } from 'react-redux';
+import { FullscreenButton } from '@src/components/fullscreen-button';
+import { loginUser } from '@src/store/auth/auth';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
-type LoginFormProps = {} & Pick<RouteComponentProps, 'match' | 'history'>;
+type LoginFormProps = {
+    handleSubmit?: (data: any) => void;
+    authError: string | null;
+    loading: boolean;
+} & Pick<RouteComponentProps, 'match' | 'history'>;
+
 type LoginFormState = {};
 
-class LoginForm extends React.PureComponent<LoginFormProps, LoginFormState> {
+const defaultProps = {
+    authError: null,
+    loading: false,
+};
+
+export class LoginForm extends React.PureComponent<LoginFormProps, LoginFormState> {
+    static defaultProps = defaultProps;
+
     constructor(props: LoginFormProps) {
         super(props);
     }
+
+    handleSubmit = (data: any) => {
+        console.log('FORMDATA', data);
+        if (this.props.handleSubmit) {
+            this.props.handleSubmit(data);
+        }
+        return false;
+    };
+
     render() {
-        const thanks = false;
-        const { history, match } = this.props;
+        return (
+            <div className="login-page-container">
+                <div className="login-page">
+                    <img src={contredanseLogo} alt="Contredanse logo" />
+
+                    <h2>Please login to your contredanse account !</h2>
+                    <p>Here some blah-blah</p>
+
+                    <Formik
+                        initialValues={{ email: '' }}
+                        onSubmit={(values, { setSubmitting }) => {
+                            this.handleSubmit(values);
+                            setSubmitting(false);
+                            /*
+                            setTimeout(() => {
+                                alert(JSON.stringify(values, null, 2));
+                                setSubmitting(false);
+                            }, 1500);
+                            */
+                        }}
+                        validationSchema={Yup.object().shape({
+                            email: Yup.string()
+                                .email()
+                                .required('Required'),
+                        })}
+                    >
+                        {props => {
+                            const {
+                                values,
+                                touched,
+                                errors,
+                                dirty,
+                                isSubmitting,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                                handleReset,
+                            } = props;
+                            return (
+                                <form onSubmit={handleSubmit}>
+                                    <label htmlFor="email" style={{ display: 'block' }}>
+                                        Email
+                                    </label>
+                                    <input
+                                        id="email"
+                                        placeholder="Enter your email"
+                                        type="text"
+                                        value={values.email}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className={errors.email && touched.email ? 'text-input error' : 'text-input'}
+                                    />
+                                    {errors.email && touched.email && (
+                                        <div className="input-feedback">{errors.email}</div>
+                                    )}
+
+                                    <button
+                                        type="button"
+                                        className="outline"
+                                        onClick={handleReset}
+                                        disabled={!dirty || isSubmitting}
+                                    >
+                                        Reset
+                                    </button>
+                                    <button type="submit" disabled={isSubmitting}>
+                                        Submit
+                                    </button>
+                                </form>
+                            );
+                        }}
+                    </Formik>
+                </div>
+            </div>
+        );
+    }
+
+    render3() {
+        const { history, match, authError } = this.props;
 
         return (
             <div className="login-page-container">
@@ -20,15 +125,36 @@ class LoginForm extends React.PureComponent<LoginFormProps, LoginFormState> {
                     <img src={contredanseLogo} alt="Contredanse logo" />
                     <h2>Please login to your contredanse account !</h2>
                     <p>Here some blah-blah</p>
-                    <div className="form">
+
+                    {authError && <div className="error-message">{authError}</div>}
+
+                    <form className="form" onSubmit={this.handleSubmit}>
                         <input type="text" placeholder="Enter email address" />
                         <input type="text" placeholder="My  password" />
-                        <button onClick={() => history.push('/')}>Login</button>
-                    </div>
+                        <button type="submit">Login</button>
+                    </form>
                 </div>
             </div>
         );
     }
 }
 
-export default LoginForm;
+const mapStateToProps = ({ auth }: ApplicationState) => ({
+    authError: auth.authError,
+    loading: auth.loading,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch): Pick<LoginFormProps, 'handleSubmit'> => ({
+    //handleSubmit: (data: any) => dispatch(uiActions.setFullscreen(isFullscreen)),
+    handleSubmit: (data: any) =>
+        loginUser(data, () => {
+            alert('onSuccess');
+        })(dispatch),
+});
+
+const ConnectedLoginForm = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(LoginForm);
+
+export default ConnectedLoginForm;
--- src/js/containers/login-container.tsx
+++ src/js/containers/login-container.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import { LoginLayout } from '@src/components/layout/login-layout';
 import LoginForm from '@src/components/login-form';
+import ConnectedLoginForm from '@src/components/login-form';
 
 type LoginContainerProps = {} & RouteComponentProps<{}>;
 type LoginContainerState = {};
@@ -13,7 +14,7 @@ class LoginContainer extends Component<LoginContainerProps, LoginContainerState>
     render() {
         return (
             <LoginLayout>
-                <LoginForm match={this.props.match} history={this.props.history} />
+                <ConnectedLoginForm match={this.props.match} history={this.props.history} />
             </LoginLayout>
         );
     }
