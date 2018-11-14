c5e9e1ad6852d8c9b6f33ce1ace8f8dbeb1d1096
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-11-14 18:10:25 +0100

    example user service

---
+++ src/js/services/user_service.ts
@@ -0,0 +1,64 @@
+import { appConfig } from '@config/config';
+import authHeader from '../helpers/auth-headers';
+
+const login = (username: string, password: string): Promise<Response> => {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+    };
+
+    return fetch(`${appConfig.getApiBaseUrl()}/users/authenticate`, requestOptions)
+        .then(handleResponse)
+        .then(user => {
+            // login successful if there's a jwt token in the response
+            if (user.token) {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('user', JSON.stringify(user));
+            }
+            return user;
+        });
+};
+
+const logout = (): void => {
+    // remove user from local storage to log user out
+    localStorage.removeItem('user');
+};
+
+const isLogged = (): any | null => {
+    const user = JSON.parse(localStorage.getItem('user') || '');
+    return user && user.token;
+};
+
+/*
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${appConfig.getApiBaseUrl()}/users`, requestOptions).then(handleResponse);
+}*/
+
+const handleResponse = (response: Response) => {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                // auto logout if 401 response returned from api
+                logout();
+                location.reload(true);
+            }
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+        return data;
+    });
+};
+
+export const userService = {
+    login,
+    logout,
+    isLogged,
+    //getAll
+};
