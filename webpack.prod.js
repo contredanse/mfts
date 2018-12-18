926fd9e7beaa30c83c711c37a48dc44a11612d79
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-12-18 14:53:52 +0100

    added node_modules compilation

--- webpack.prod.js
+++ webpack.prod.js
@@ -75,6 +75,27 @@ module.exports = merge(common, {
                     name: 'static/fonts/[name].[ext]',
                 },
             },
+            // Process any JS outside of the app with Babel.
+            // Unlike the application JS, we only compile the standard ES features.
+            {
+                test: /\.(js|mjs)$/,
+                exclude: /@babel(?:\/|\\{1,2})runtime/,
+                loader: require.resolve('babel-loader'),
+                options: {
+                    babelrc: false,
+                    configFile: false,
+                    compact: false,
+                    presets: [[require.resolve('babel-preset-react-app/dependencies'), { helpers: true }]],
+                    cacheDirectory: true,
+                    // cacheCompression: true,
+
+                    // If an error happens in a package, it's possible to be
+                    // because it was compiled. Thus, we don't want the browser
+                    // debugger to show the original code. Instead, the code
+                    // being evaluated would be much more helpful.
+                    sourceMaps: false,
+                },
+            },
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 use: [
