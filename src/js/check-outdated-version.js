dad154e1d4b315202e8832a1450601a3488b57df
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-12-19 12:04:16 +0100

    Updated constraints

--- src/js/check-outdated-version.js
+++ src/js/check-outdated-version.js
@@ -3,14 +3,14 @@ var outdatedBrowserRework = require('outdated-browser-rework');
 outdatedBrowserRework({
     browserSupport: {
         Chrome: 57, // Includes Chrome for mobile devices
-        Edge: 15,
-        Safari: 11.1,
-        'Mobile Safari': 10,
-        Firefox: 50,
+        Edge: 16,
+        // allows 11.1 as minimum
+        Safari: 11,
+        // allows 10.3 as minimum (grid and fetch support)
+        'Mobile Safari': 10.2,
+        Firefox: 54,
         Opera: 50,
         Vivaldi: 1,
-        // You could specify a version here if you still support IE in 2017.
-        // You could also instead seriously consider what you're doing with your time and budget
         IE: false,
     },
     requireChromeOnAndroid: false,
