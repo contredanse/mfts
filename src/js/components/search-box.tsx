3d1df5b94f7fa8beded07d76d56591fca26f3894
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-12-19 16:04:08 +0100

    Added translations

--- src/js/components/search-box.tsx
+++ src/js/components/search-box.tsx
@@ -4,22 +4,28 @@ import './search-box.scss';
 type SearchBoxProps = {
     placeHolder?: string;
     value?: string;
+    lang?: string;
 };
 
 type SearchBoxState = {};
+
+const defaultProps = {
+    lang: 'en',
+};
 
 export class SearchBox extends React.Component<
     SearchBoxProps & React.HTMLAttributes<HTMLInputElement>,
     SearchBoxState
 > {
-    static defaultProps = {
-        placeHolder: 'search',
-    };
+    static defaultProps = defaultProps;
     render() {
-        const { placeHolder, value, ...restProps } = this.props;
+        const { placeHolder, value, lang, ...restProps } = this.props;
+
+        const searchLabel = placeHolder ? placeHolder : lang === 'fr' ? 'Rechercher' : 'Search';
+
         return (
             <div className="search-box-ctn">
-                <input className="search" value={value} type="search" placeholder={placeHolder} {...restProps} />
+                <input className="search" value={value} type="search" placeholder={searchLabel} {...restProps} />
             </div>
         );
     }
