9486dea626c0d2cc3c25964b1e6be1bb918ba35a
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-12-21 14:29:16 +0100

    removed suspense api

--- src/js/containers/app.tsx
+++ src/js/containers/app.tsx
@@ -1,4 +1,5 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
+//import React, { lazy, Suspense } from 'react';
 import { hot } from 'react-hot-loader';
 
 import { Route } from 'react-router-dom';
@@ -7,16 +8,34 @@ import { ConnectedRouter } from 'connected-react-router';
 
 import history from '@src/history';
 import { ConnectedAppBar } from '@src/components/navigation/app-bar';
-import HomeContainer from '@src/containers/home-container';
-import NotFoundContainer from '@src/containers/notfound-container';
 import AppConfig from '@src/core/app-config';
-
-//import LoginContainer from '@src/containers/login-container';
 import DocumentMeta from '@src/utils/document-meta';
 import { ConnectedFullScreen } from '@src/utils/fullscreen';
-
 import { ConnectedSideMenu } from '@src/components/navigation/side-menu';
 import { LanguageContextConsumer, LanguageContextProvider } from '@src/context/language-context';
+import HomeContainer from './home-container';
+import NotFoundContainer from './notfound-container';
+import LoginContainer from './login-container';
+import MenuContainer from './menu-container';
+import PageContainer from './page-container';
+import PageListContainer from './page-list-container';
+import WelcomeContainer from './welcome-container';
+import IntroContainer from './intro-container';
+import AboutContainer from './about-container';
+/**
+ * For Suspense support, Warning this path is abandonned
+ * Makes the deploy/hash more complicated as old resources
+ * must still be present when loading a new page.
+ */
+
+//const LoadingMessage: React.FC = () => <div>...</div>;
+//const LoginContainer = lazy(() => import('./login-container'));
+//const IntroContainer = lazy(() => import('./intro-container'));
+//const PageContainer = lazy(() => import('./page-container'));
+//const PageListContainer = lazy(() => import('./page-list-container'));
+//const AboutContainer = lazy(() => import('./about-container'));
+//const MenuContainer = lazy(() => import('./menu-container'));
+//const WelcomeContainer = lazy(() => import('./welcome-container'));
 
 type AppProps = {
     appConfig: AppConfig;
@@ -29,22 +48,6 @@ type AppState = {
 const defaultState = {
     isFullscreen: false,
 };
-
-const LoadingMessage: React.FC = () => <div>...</div>;
-
-const LoginContainer = lazy(() => import('./login-container'));
-
-const IntroContainer = lazy(() => import('./intro-container'));
-
-const PageContainer = lazy(() => import('./page-container'));
-
-const PageListContainer = lazy(() => import('./page-list-container'));
-
-const AboutContainer = lazy(() => import('./about-container'));
-
-const MenuContainer = lazy(() => import('./menu-container'));
-
-const WelcomeContainer = lazy(() => import('./welcome-container'));
 
 class App extends React.PureComponent<AppProps, AppState> {
     readonly state: AppState;
@@ -65,89 +68,83 @@ class App extends React.PureComponent<AppProps, AppState> {
             const lang = (match.params! as { lang: string }).lang;
             return (
                 <Switch>
-                    <Suspense fallback={<LoadingMessage />}>
-                        <Route
-                            exact={true}
-                            path={`${match.path}/menu/:pageId?`}
-                            render={(props: RouteComponentProps<any>) => {
-                                const { pageId, lang: routeLang } = props.match.params;
-                                return (
-                                    <MenuContainer lang={lang} menuRepository={menuRepository} openedPageId={pageId} />
-                                );
-                            }}
-                        />
-                        <Route
-                            exact={true}
-                            path={`${match.path}/intro`}
-                            render={() => {
-                                return <IntroContainer lang={lang} pageRepository={pageRepository} />;
-                            }}
-                        />
-                        <Route
-                            exact={true}
-                            path={`${match.path}/welcome/:pageId?`}
-                            render={(props: RouteComponentProps<any>) => {
-                                const { pageId, lang: routeLang } = props.match.params;
-                                return (
-                                    <WelcomeContainer
-                                        lang={routeLang}
-                                        pageRepository={pageRepository}
-                                        fromPageId={pageId}
-                                    />
-                                );
-                            }}
-                        />
-                        <Route
-                            exact={true}
-                            path={`${match.path}/page-list/:menuId?`}
-                            render={(props: RouteComponentProps<any>) => {
-                                const { lang: routeLang, menuId } = props.match.params;
-                                return (
-                                    <PageListContainer
-                                        lang={routeLang}
-                                        menuId={menuId}
-                                        videosBaseUrl={assetsLocator.getMediaTypeBaseUrl('videos')}
-                                        pageRepository={pageRepository}
-                                        {...props}
-                                    />
-                                );
-                            }}
-                        />
-                        <Route
-                            exact={true}
-                            path={`${match.path}/page/:pageId`}
-                            render={(props: RouteComponentProps<any>) => {
-                                const { pageId, lang: routeLang } = props.match.params;
-                                return (
-                                    <PageContainer
-                                        pageId={pageId}
-                                        lang={routeLang || lang}
-                                        pageRepository={pageRepository}
-                                        menuRepository={menuRepository}
-                                        {...props}
-                                    />
-                                );
-                            }}
-                        />
-                        <Route
-                            exact={true}
-                            path={`${match.path}/about/:section`}
-                            render={(props: RouteComponentProps<any>) => {
-                                const { section, lang: routeLang } = props.match.params;
-                                return (
-                                    <AboutContainer assetsLocator={assetsLocator} lang={routeLang} section={section} />
-                                );
-                            }}
-                        />
+                    <Route
+                        exact={true}
+                        path={`${match.path}/menu/:pageId?`}
+                        render={(props: RouteComponentProps<any>) => {
+                            const { pageId, lang: routeLang } = props.match.params;
+                            return <MenuContainer lang={lang} menuRepository={menuRepository} openedPageId={pageId} />;
+                        }}
+                    />
+                    <Route
+                        exact={true}
+                        path={`${match.path}/intro`}
+                        render={() => {
+                            return <IntroContainer lang={lang} pageRepository={pageRepository} />;
+                        }}
+                    />
+                    <Route
+                        exact={true}
+                        path={`${match.path}/welcome/:pageId?`}
+                        render={(props: RouteComponentProps<any>) => {
+                            const { pageId, lang: routeLang } = props.match.params;
+                            return (
+                                <WelcomeContainer
+                                    lang={routeLang}
+                                    pageRepository={pageRepository}
+                                    fromPageId={pageId}
+                                />
+                            );
+                        }}
+                    />
+                    <Route
+                        exact={true}
+                        path={`${match.path}/page-list/:menuId?`}
+                        render={(props: RouteComponentProps<any>) => {
+                            const { lang: routeLang, menuId } = props.match.params;
+                            return (
+                                <PageListContainer
+                                    lang={routeLang}
+                                    menuId={menuId}
+                                    videosBaseUrl={assetsLocator.getMediaTypeBaseUrl('videos')}
+                                    pageRepository={pageRepository}
+                                    {...props}
+                                />
+                            );
+                        }}
+                    />
+                    <Route
+                        exact={true}
+                        path={`${match.path}/page/:pageId`}
+                        render={(props: RouteComponentProps<any>) => {
+                            const { pageId, lang: routeLang } = props.match.params;
+                            return (
+                                <PageContainer
+                                    pageId={pageId}
+                                    lang={routeLang || lang}
+                                    pageRepository={pageRepository}
+                                    menuRepository={menuRepository}
+                                    {...props}
+                                />
+                            );
+                        }}
+                    />
+                    <Route
+                        exact={true}
+                        path={`${match.path}/about/:section`}
+                        render={(props: RouteComponentProps<any>) => {
+                            const { section, lang: routeLang } = props.match.params;
+                            return <AboutContainer assetsLocator={assetsLocator} lang={routeLang} section={section} />;
+                        }}
+                    />
 
-                        <Route
-                            exact={true}
-                            path={`${match.path}/login`}
-                            render={() => {
-                                return <LoginContainer lang={lang} />;
-                            }}
-                        />
-                    </Suspense>
+                    <Route
+                        exact={true}
+                        path={`${match.path}/login`}
+                        render={() => {
+                            return <LoginContainer lang={lang} />;
+                        }}
+                    />
                     <Route component={NotFoundContainer} />
                 </Switch>
             );
