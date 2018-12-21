c9b01cf22a39cfa6e60e7889968d6514d4de6e13
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-12-21 23:37:22 +0100

    prep add to homescreen

--- src/js/components/pwa-installer.tsx
+++ src/js/components/pwa-installer.tsx
@@ -8,16 +8,18 @@ type Props = {
 
 type State = {
     installed: boolean;
+    cancelled: boolean;
 };
 
 const defaultProps = {
     displayTimeout: 30,
 };
 
-class PwaInstaller extends React.Component<Props, State> {
+class PwaInstaller extends React.PureComponent<Props, State> {
     static defaultProps = defaultProps;
     readonly state = {
         installed: true,
+        cancelled: false,
     };
 
     deferredPrompt?: BeforeInstallPromptEvent;
@@ -47,8 +49,16 @@ class PwaInstaller extends React.Component<Props, State> {
                 console.log(choiceResult.outcome);
                 if (choiceResult.outcome === 'dismissed') {
                     console.log('User cancelled home screen install');
+                    this.setState({
+                        cancelled: true,
+                        installed: false,
+                    });
                 } else {
                     console.log('User added to home screen');
+                    this.setState({
+                        cancelled: false,
+                        installed: true,
+                    });
                 }
                 // We no longer need the prompt.  Clear it up.
                 this.deferredPrompt = undefined;
@@ -57,14 +67,14 @@ class PwaInstaller extends React.Component<Props, State> {
     };
 
     render() {
-        const { installed } = this.state;
-        //const installed = false;
+        const { installed, cancelled } = this.state;
+        if (installed || cancelled) {
+            return null;
+        }
         return (
-            !installed && (
-                <div className="add-to-homescreen-container">
-                    <button onClick={this.requestAddToHomescreen}>Add to homescreen</button>
-                </div>
-            )
+            <div className="add-to-homescreen-container">
+                <button onClick={this.requestAddToHomescreen}>Add to homescreen</button>
+            </div>
         );
     }
 }
