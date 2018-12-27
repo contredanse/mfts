33ae7571dbc0cd0d0e0dd08f1c6321f300b8e344
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-12-27 16:49:31 +0100

    lang prop

--- src/js/components/home.tsx
+++ src/js/components/home.tsx
@@ -91,7 +91,7 @@ class Home extends React.PureComponent<HomeProps, HomeState> {
 
         return (
             <div className="home-container">
-                <PwaInstaller />
+                <PwaInstaller lang={lang} />
                 <ConnectedLangSelector>
                     {({ currentLang, nextLang, toggleLang, updateLang }) => (
                         <div className="lang-selector-panel" onClick={() => updateLang(nextLang)}>
