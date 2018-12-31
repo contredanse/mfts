8eb311e8f98bab074ac0ed33580805a2e17acf8b
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-12-31 14:35:01 +0100

    fixed cors in dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -112,6 +112,8 @@ module.exports = merge(common, {
             },
         },
         headers: {
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
             'Access-Control-Allow-Origin': '*',
         },
     },
