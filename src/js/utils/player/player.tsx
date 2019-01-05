1e9a656e0000ac357318894a8237f5806a4a7f34
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-01-05 22:20:19 +0100

    removed obsolete interface

--- src/js/utils/player/player.tsx
+++
@@ -1,6 +0,0 @@
-export type PlayerActions = {
-    pause: () => void;
-    play: () => void;
-    setPlaybackRate: (playbackRate: number) => void;
-    setCurrentTime: (currentTime: number) => void;
-};
