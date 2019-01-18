300fe3fb6ef3af8775d0d6099c80c32726ff06d7
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-01-18 14:13:54 +0100

    cleanup console

--- src/js/helpers/main-menu-redirect.ts
+++ src/js/helpers/main-menu-redirect.ts
@@ -8,15 +8,11 @@ export const helixMenuIconMinimumHeight = 400;
 export const isScreenAdaptedForHelixMenu = (): boolean => {
     const screenWidth = window.innerWidth || window.screen.width;
     const screenHeight = window.innerHeight || window.screen.height;
-    console.log('screenHeight', screenHeight);
-    console.log('screenWidth', screenWidth);
-    console.log('test', screenWidth >= helixMenuIconMinimumWidth && screenHeight >= helixMenuIconMinimumHeight);
     return screenWidth >= helixMenuIconMinimumWidth && screenHeight >= helixMenuIconMinimumHeight;
 };
 
 export const getMainMenuRoute = (lang: string, pageId?: string): string => {
     const menuId = isScreenAdaptedForHelixMenu() ? 'menu' : 'page-list';
-    console.log('menuId', menuId);
     if (pageId) {
         return `/${lang}/${menuId}/${pageId}`;
     }
