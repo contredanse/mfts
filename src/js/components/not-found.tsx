ab102ad2245f46934aeb0ceab0cb48fd728c2d50
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-01-19 12:22:55 +0100

    Added keyframe

--- src/js/components/not-found.tsx
+++ src/js/components/not-found.tsx
@@ -10,7 +10,7 @@ const NotFound: React.FC<NotFoundProps> = props => {
     return (
         <div className="not-found-container">
             <div className="not-found-inner">
-                <h2>Oops, sorry this page was not found.</h2>
+                <h2 className="text-focus-in">Oops, sorry this page was not found.</h2>
             </div>
         </div>
     );
